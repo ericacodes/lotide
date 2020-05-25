62eddec633b0feabbb002ed9095c48917c095ec0
Erica Sun <ericasun@seas.upenn.edu>
2020-05-25 20:45:04 +0000

    head.js return the first element in an array

--- head.js
+++ head.js
@@ -6,7 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("LighthouseLabs", "LighthouseLabs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);a
-assertEqual(1, 2);
+function head(array) {
+  return array[0];
+}
+
+assertEqual(head([1, 3, 5]), 1);
+assertEqual(head(["hello", "erica"]), "hello");
+assertEqual(head(["one element"]), "one element");
+assertEqual(head([]), undefined);
