c25a57a02915b95ca96005c5d928eab169e0892a
Erica Sun <ericasun@seas.upenn.edu>
2020-05-27 20:49:42 +0000

    implemented findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let answer;
+  let keys = Object.keys(object);
+
+  for (const key of keys) {
+    if (object[key] === value) {
+      answer = key;
+      break;
+    }
+  }
+  
+  return answer;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+};
+
+const emptyTVShowList = {};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(emptyTVShowList, "That '70s Show"), undefined);
+
+const keysWithSameValue = {
+  key1: "hello",
+  key2: "world",
+  key3: "hello"
+};
+
+assertEqual(findKeyByValue(keysWithSameValue, "hello"), "key1");
