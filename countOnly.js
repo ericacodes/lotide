a106027438549273f9e0d8a9e4625120a9469bdb
Erica Sun <ericasun@seas.upenn.edu>
2020-05-27 19:32:10 +0000

    linted

--- countOnly.js
+++ countOnly.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
 
   for (const item of allItems) {
     if (itemsToCount[item]) {
@@ -21,8 +21,8 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   // console.log(results);
-  return results; 
-}
+  return results;
+};
 
 const firstNames = [
   "Karl",
