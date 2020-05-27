a18a38bfc2a72986e690cf346a1810e79f222613
Erica Sun <ericasun@seas.upenn.edu>
2020-05-27 19:08:29 +0000

    refactored using ternary operator

--- middle.js
+++ middle.js
@@ -15,22 +15,21 @@ const assertArraysEqual = function(array1, array2) {
     console.log("✅✅✅ Assertion Passed");
   } else {
     console.log("❌❌❌ Assertion Failed");
-    // console.log("❌❌❌ Assertion Failed: " + array1 + " !== " + array2);
+    //console.log("❌❌❌ Assertion Failed: " + array1 + " !== " + array2);
     // console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
 const middle = function(array) {
+  // if array has one of two elements, return empty array
   if (array.length < 3) {
     return [];
-  } else {
-    let index = Math.ceil(array.length / 2) - 1;
-    if (array.length % 2 === 0) {
-      return array.splice(index, 2);
-    } else {
-      return array.splice(index, 1);
-    }
   }
+  let index = Math.ceil(array.length / 2) - 1;
+  // otherwise, return middle elements in new array 
+  return (!(array.length % 2))  // returns a number which has a truthy/falsy value 
+    ? array.splice(index, 2)
+    : array.splice(index, 1);
 };
 
 assertArraysEqual(middle([1]), []);
