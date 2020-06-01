f0eea378517899b6b45e28220cdd0ce0c2dbe9d5
Erica Sun <ericasun@seas.upenn.edu>
2020-06-01 19:44:36 +0000

    set up lotide with M&C and converted headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([1, 3, 5]), 1);
-assertEqual(head(["hello", "erica"]), "hello");
-assertEqual(head(["one element"]), "one element");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  }); 
+  it("returns 'hello' for ['hello', 'erica'])", () => {
+    assert.strictEqual(head(['hello', 'erica']), 'hello');
+  });
+  it("returns 'one element' for ['one element']", () => {
+    assert.strictEqual(head(['one element']), 'one element');
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
